05e3f0bc41954af3aa20c26cf1b10f28bcf0a85d
Thomas Lekanger <thomas.lekanger@hotmail.com>
2024-10-13 18:26:34 +0200

    fix: add chrome 124 support

--- src/presets.ts
+++ src/presets.ts
@@ -9,7 +9,7 @@ export type PresetMap = {
     safari: SafariPresetVersion;
 };
 
-export type ChromePresetVersion = "107" | "110" | "116";
+export type ChromePresetVersion = "107" | "110" | "116" | "124";
 
 export const ChromePresets: Record<ChromePresetVersion, Preset> = {
     "107": {
@@ -88,6 +88,39 @@ export const ChromePresets: Record<ChromePresetVersion, Preset> = {
             "--http2",
             "--http2-no-server-push",
             "--compressed",
+            "--tlsv1.2",
+            "--alps",
+            "--tls-permute-extensions",
+            "--cert-compression brotli",
+            "--tls-grease"
+        ]
+    },
+    "124": {
+        headers: {
+            "sec-ch-ua": `"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"`,
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "macOS",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS    X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+            Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "en-US,en;q=0.9",
+            Priority: "u=0, i"
+        },
+        flags: [
+            "--ciphers TLS_AES_128_GCM_SHA256,TLS_AES_256_GCM_SHA384,TLS_CHACHA20_POLY1305_SHA256,ECDHE-ECDSA-AES128-GCM-SHA256,ECDHE-RSA-AES128-GCM-SHA256,ECDHE-ECDSA-AES256-GCM-SHA384,ECDHE-RSA-AES256-GCM-SHA384,ECDHE-ECDSA-CHACHA20-POLY1305,ECDHE-RSA-CHACHA20-POLY1305,ECDHE-RSA-AES128-SHA,ECDHE-RSA-AES256-SHA,AES128-GCM-SHA256,AES256-GCM-SHA384,AES128-SHA,AES256-SHA",
+            "--curves X25519Kyber768Draft00:X25519:P-256:P-384",
+            "--http2",
+            "--http2-settings '1:65536;2:0;4:6291456;6:262144'",
+            "--http2-window-update 15663105",
+            "--http2-stream-weight 256",
+            "--http2-stream-exclusive 1",
+            "--compressed",
+            "--ech GREASE",
             "--tlsv1.2",
             "--alps",
             "--tls-permute-extensions",
