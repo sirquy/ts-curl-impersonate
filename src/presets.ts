a4023609d9e9b1445d650c003acfcb1d6118bba9
Thomas Lekanger <thomas.lekanger@hotmail.com>
2024-10-14 19:19:45 +0200

    Update presets.ts

--- src/presets.ts
+++ src/presets.ts
@@ -101,7 +101,7 @@ export const ChromePresets: Record<ChromePresetVersion, Preset> = {
             "sec-ch-ua-mobile": "?0",
             "sec-ch-ua-platform": "macOS",
             "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS    X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
             Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "Sec-Fetch-Site": "none",
             "Sec-Fetch-Mode": "navigate",
